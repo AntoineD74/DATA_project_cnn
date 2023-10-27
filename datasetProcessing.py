bd3c4dde91e338bd8e15373f7f8f2c86db25d72d
antoine dupuis <antoine.dupuis74@icloud.com>
2023-10-27 17:15:51 +0000

    Remove unused file

---
+++ datasetProcessing.py
@@ -0,0 +1,290 @@
+"""
+    Source for data preprocessing: https://www.kaggle.com/code/gusthema/asl-fingerspelling-recognition-w-tensorflow/notebook
+"""
+
+
+import os
+import shutil
+import matplotlib
+from matplotlib import animation, rc
+from tqdm.notebook import tqdm
+import numpy as np
+import pandas as pd
+import pyarrow.parquet as pq
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import mediapipe
+from mediapipe.framework.formats import landmark_pb2
+import json
+
+
+
+matplotlib.rcParams['animation.embed_limit'] = 2**128
+matplotlib.rcParams['savefig.pad_inches'] = 0
+rc('animation', html='jshtml')
+
+def create_animation(images):
+    fig = plt.figure(figsize=(6, 9))
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    im=ax.imshow(images[0], cmap="gray")
+    plt.show(fig)
+    plt.close(fig)
+    
+    def animate_func(i):
+        im.set_array(images[i])
+        return [im]
+
+    return animation.FuncAnimation(fig, animate_func, frames=len(images), interval=1000/10)
+
+
+# This function extracts the data for both hands.
+def get_hands(seq_df):
+    images = []
+    all_hand_landmarks = []
+    for seq_idx in range(len(seq_df)):
+        x_hand = seq_df.iloc[seq_idx].filter(regex="x_right_hand.*").values
+        y_hand = seq_df.iloc[seq_idx].filter(regex="y_right_hand.*").values
+        z_hand = seq_df.iloc[seq_idx].filter(regex="z_right_hand.*").values
+
+        right_hand_image = np.zeros((600, 600, 3))
+
+        right_hand_landmarks = landmark_pb2.NormalizedLandmarkList()
+        
+        for x, y, z in zip(x_hand, y_hand, z_hand):
+            right_hand_landmarks.landmark.add(x=x, y=y, z=z)
+
+        mp_drawing.draw_landmarks(
+                right_hand_image,
+                right_hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style())
+        
+        x_hand = seq_df.iloc[seq_idx].filter(regex="x_left_hand.*").values
+        y_hand = seq_df.iloc[seq_idx].filter(regex="y_left_hand.*").values
+        z_hand = seq_df.iloc[seq_idx].filter(regex="z_left_hand.*").values
+        
+        left_hand_image = np.zeros((600, 600, 3))
+        
+        left_hand_landmarks = landmark_pb2.NormalizedLandmarkList()
+        for x, y, z in zip(x_hand, y_hand, z_hand):
+            left_hand_landmarks.landmark.add(x=x, y=y, z=z)
+
+        mp_drawing.draw_landmarks(
+                left_hand_image,
+                left_hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style())
+        
+        images.append([right_hand_image.astype(np.uint8), left_hand_image.astype(np.uint8)])
+        all_hand_landmarks.append([right_hand_landmarks, left_hand_landmarks])
+    return images, all_hand_landmarks
+
+
+#images, labels, labels2=common.read_json('D:\asl-fingerspelling\character_to_prediction_index.json', 20)
+dataset_df = pd.read_csv("D:\\asl-fingerspelling\\train.csv")
+print("Full train dataset shape is {}".format(dataset_df.shape))
+
+sequence_id, file_id, phrase = dataset_df.iloc[0][['sequence_id', 'file_id', 'phrase']]
+print(f"sequence_id: {sequence_id}, file_id: {file_id}, phrase: {phrase}")
+
+# Fetch data from parquet file
+sample_sequence_df = pq.read_table(f"D:\\asl-fingerspelling/train_landmarks/{str(file_id)}.parquet",
+filters=[[('sequence_id', '=', sequence_id)],]).to_pandas()
+print("Full sequence dataset shape is {}".format(sample_sequence_df.shape))
+print(sample_sequence_df.head())
+
+# Extract the landmark data and convert it to an image using medipipe library.
+mp_pose = mediapipe.solutions.pose
+mp_hands = mediapipe.solutions.hands
+mp_drawing = mediapipe.solutions.drawing_utils 
+mp_drawing_styles = mediapipe.solutions.drawing_styles
+
+# Get the images created using mediapipe apis
+hand_images, hand_landmarks = get_hands(sample_sequence_df)
+# Fetch and show the data for right hand
+create_animation(np.array(hand_images)[:, 0])
+
+# Pose coordinates for hand movement.
+LPOSE = [13, 15, 17, 19, 21]
+RPOSE = [14, 16, 18, 20, 22]
+POSE = LPOSE + RPOSE
+
+X = [f'x_right_hand_{i}' for i in range(21)] + [f'x_left_hand_{i}' for i in range(21)] + [f'x_pose_{i}' for i in POSE]
+Y = [f'y_right_hand_{i}' for i in range(21)] + [f'y_left_hand_{i}' for i in range(21)] + [f'y_pose_{i}' for i in POSE]
+Z = [f'z_right_hand_{i}' for i in range(21)] + [f'z_left_hand_{i}' for i in range(21)] + [f'z_pose_{i}' for i in POSE]
+FEATURE_COLUMNS = X + Y + Z
+
+X_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "x_" in col]
+Y_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "y_" in col]
+Z_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "z_" in col]
+
+RHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "right" in col]
+LHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "left" in col]
+RPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in RPOSE]
+LPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in LPOSE]
+
+# Set length of frames to 128
+FRAME_LEN = 128
+
+#Treating data in the dataset
+def preprocessing():    
+    # Create directory to store the new data
+    if not os.path.isdir("D:\\asl-fingerspelling\\preprocessed"):
+        os.mkdir("D:\\asl-fingerspelling\\preprocessed")
+    else:
+        shutil.rmtree("D:\\asl-fingerspelling\\preprocessed")
+        os.mkdir("D:\\asl-fingerspelling\\preprocessed")
+    
+    # Loop through each file_id
+    for file_id in tqdm(dataset_df.file_id.unique()):
+        pq_file = f"D:\\asl-fingerspelling\\train_landmarks\\{file_id}.parquet"
+        file_df = dataset_df.loc[dataset_df["file_id"] == file_id]
+        parquet_df = pq.read_table(f"D:\\asl-fingerspelling\\train_landmarks\\{str(file_id)}.parquet",
+                                  columns=['sequence_id'] + FEATURE_COLUMNS).to_pandas()
+
+        tf_file = f"D:\\asl-fingerspelling\\preprocessed/{file_id}.tfrecord"
+        print(tf_file)
+        parquet_numpy = parquet_df.to_numpy()
+        with tf.io.TFRecordWriter(tf_file) as file_writer:
+            # Loop through each sequence in file.
+            for seq_id, phrase in zip(file_df.sequence_id, file_df.phrase):
+                # Fetch sequence data
+                frames = parquet_numpy[parquet_df.index == seq_id]
+                
+                # Calculate the number of NaN values in each hand landmark
+                r_nonan = np.sum(np.sum(np.isnan(frames[:, RHAND_IDX]), axis = 1) == 0)
+                l_nonan = np.sum(np.sum(np.isnan(frames[:, LHAND_IDX]), axis = 1) == 0)
+                no_nan = max(r_nonan, l_nonan)
+                
+                if 2*len(phrase)<no_nan:
+                    features = {FEATURE_COLUMNS[i]: tf.train.Feature(
+                        float_list=tf.train.FloatList(value=frames[:, i])) for i in range(len(FEATURE_COLUMNS))}
+                    features["phrase"] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(phrase, 'utf-8')]))
+                    record_bytes = tf.train.Example(features=tf.train.Features(feature=features)).SerializeToString()
+                    file_writer.write(record_bytes)
+                    
+    tf_records = dataset_df.file_id.map(lambda x: f'D:\\asl-fingerspelling\\preprocessed\\{x}.tfrecord').unique()
+    print(f"List of {len(tf_records)} TFRecord files.")
+
+
+# Function to resize and add padding.
+def resize_pad(x):
+    if tf.shape(x)[0] < FRAME_LEN:
+        x = tf.pad(x, ([[0, FRAME_LEN-tf.shape(x)[0]], [0, 0], [0, 0]]))
+    else:
+        x = tf.image.resize(x, (FRAME_LEN, tf.shape(x)[1]))
+    return x
+
+
+def decode_fn(record_bytes):
+    schema = {COL: tf.io.VarLenFeature(dtype=tf.float32) for COL in FEATURE_COLUMNS}
+    schema["phrase"] = tf.io.FixedLenFeature([], dtype=tf.string)
+    features = tf.io.parse_single_example(record_bytes, schema)
+    phrase = features["phrase"]
+    landmarks = ([tf.sparse.to_dense(features[COL]) for COL in FEATURE_COLUMNS])
+    # Transpose to maintain the original shape of landmarks data.
+    landmarks = tf.transpose(landmarks)
+    
+    return landmarks, phrase
+
+def convert_fn(landmarks, phrase):
+    # Add start and end pointers to phrase.
+    phrase = start_token + phrase + end_token
+    phrase = tf.strings.bytes_split(phrase)
+    phrase = table.lookup(phrase)
+    
+    # Vectorize and add padding.
+    phrase = tf.pad(phrase, paddings=[[0, 64 - tf.shape(phrase)[0]]], mode = 'CONSTANT', constant_values = pad_token_idx)
+    # Apply pre_process function to the landmarks.
+    return pre_process(landmarks), phrase
+
+
+# Detect the dominant hand from the number of NaN values.
+# Dominant hand will have less NaN values since it is in frame moving.
+def pre_process(x):
+    rhand = tf.gather(x, RHAND_IDX, axis=1)
+    lhand = tf.gather(x, LHAND_IDX, axis=1)
+    rpose = tf.gather(x, RPOSE_IDX, axis=1)
+    lpose = tf.gather(x, LPOSE_IDX, axis=1)
+    
+    rnan_idx = tf.reduce_any(tf.math.is_nan(rhand), axis=1)
+    lnan_idx = tf.reduce_any(tf.math.is_nan(lhand), axis=1)
+    
+    rnans = tf.math.count_nonzero(rnan_idx)
+    lnans = tf.math.count_nonzero(lnan_idx)
+    
+    # For dominant hand
+    if rnans > lnans:
+        hand = lhand
+        pose = lpose
+        
+        hand_x = hand[:, 0*(len(LHAND_IDX)//3) : 1*(len(LHAND_IDX)//3)]
+        hand_y = hand[:, 1*(len(LHAND_IDX)//3) : 2*(len(LHAND_IDX)//3)]
+        hand_z = hand[:, 2*(len(LHAND_IDX)//3) : 3*(len(LHAND_IDX)//3)]
+        hand = tf.concat([1-hand_x, hand_y, hand_z], axis=1)
+        
+        pose_x = pose[:, 0*(len(LPOSE_IDX)//3) : 1*(len(LPOSE_IDX)//3)]
+        pose_y = pose[:, 1*(len(LPOSE_IDX)//3) : 2*(len(LPOSE_IDX)//3)]
+        pose_z = pose[:, 2*(len(LPOSE_IDX)//3) : 3*(len(LPOSE_IDX)//3)]
+        pose = tf.concat([1-pose_x, pose_y, pose_z], axis=1)
+    else:
+        hand = rhand
+        pose = rpose
+    
+    hand_x = hand[:, 0*(len(LHAND_IDX)//3) : 1*(len(LHAND_IDX)//3)]
+    hand_y = hand[:, 1*(len(LHAND_IDX)//3) : 2*(len(LHAND_IDX)//3)]
+    hand_z = hand[:, 2*(len(LHAND_IDX)//3) : 3*(len(LHAND_IDX)//3)]
+    hand = tf.concat([hand_x[..., tf.newaxis], hand_y[..., tf.newaxis], hand_z[..., tf.newaxis]], axis=-1)
+    
+    mean = tf.math.reduce_mean(hand, axis=1)[:, tf.newaxis, :]
+    std = tf.math.reduce_std(hand, axis=1)[:, tf.newaxis, :]
+    hand = (hand - mean) / std
+
+    pose_x = pose[:, 0*(len(LPOSE_IDX)//3) : 1*(len(LPOSE_IDX)//3)]
+    pose_y = pose[:, 1*(len(LPOSE_IDX)//3) : 2*(len(LPOSE_IDX)//3)]
+    pose_z = pose[:, 2*(len(LPOSE_IDX)//3) : 3*(len(LPOSE_IDX)//3)]
+    pose = tf.concat([pose_x[..., tf.newaxis], pose_y[..., tf.newaxis], pose_z[..., tf.newaxis]], axis=-1)
+    
+    x = tf.concat([hand, pose], axis=1)
+    x = resize_pad(x)
+    
+    x = tf.where(tf.math.is_nan(x), tf.zeros_like(x), x)
+    x = tf.reshape(x, (FRAME_LEN, len(LHAND_IDX) + len(LPOSE_IDX)))
+    return x
+
+
+with open ("D:\\asl-fingerspelling/character_to_prediction_index.json", "r") as f:
+    char_to_num = json.load(f)
+
+# Add pad_token, start pointer and end pointer to the dict
+pad_token = 'P'
+start_token = '<'
+end_token = '>'
+pad_token_idx = 59
+start_token_idx = 60
+end_token_idx = 61
+
+char_to_num[pad_token] = pad_token_idx
+char_to_num[start_token] = start_token_idx
+char_to_num[end_token] = end_token_idx
+num_to_char = {j:i for i,j in char_to_num.items()}
+table = tf.lookup.StaticHashTable(
+    initializer=tf.lookup.KeyValueTensorInitializer(
+        keys=list(char_to_num.keys()),
+        values=list(char_to_num.values()),
+    ),
+    default_value=tf.constant(-1),
+    name="class_weight"
+)
+
+
+
+# Creating final dataset
+tf_records = dataset_df.file_id.map(lambda x: f'D:\\asl-fingerspelling\\preprocessed\\{x}.tfrecord').unique()
+batch_size = 64
+train_len = int(0.8 * len(tf_records))
+
+train_ds = tf.data.TFRecordDataset(tf_records[:train_len]).map(decode_fn).map(convert_fn).batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE).cache()
+valid_ds = tf.data.TFRecordDataset(tf_records[train_len:]).map(decode_fn).map(convert_fn).batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE).cache()
--- train.py
+++ train.py
@@ -1,169 +1,64 @@
-import os
-import shutil
-import matplotlib
-from matplotlib import animation, rc
-from tqdm.notebook import tqdm
-import numpy as np
-import pandas as pd
-import pyarrow.parquet as pq
-import matplotlib.pyplot as plt
+import transformer
+import datasetProcessing as dp
 import tensorflow as tf
-import mediapipe
-from mediapipe.framework.formats import landmark_pb2
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.optimizers import Adadelta, Adam, Adamax, Lion, SGD
 
 
+batch = next(iter(dp.valid_ds))
 
-matplotlib.rcParams['animation.embed_limit'] = 2**128
-matplotlib.rcParams['savefig.pad_inches'] = 0
-rc('animation', html='jshtml')
+# The vocabulary to convert predicted indices into characters
+idx_to_char = list(dp.char_to_num.keys())
+display_cb = transformer.DisplayOutputs(batch, idx_to_char, target_start_token_idx=dp.char_to_num['<'], target_end_token_idx=dp.char_to_num['>'])
 
-def create_animation(images):
-    fig = plt.figure(figsize=(6, 9))
-    ax = plt.Axes(fig, [0., 0., 1., 1.])
-    ax.set_axis_off()
-    fig.add_axes(ax)
-    im=ax.imshow(images[0], cmap="gray")
-    plt.show(fig)
-    plt.close(fig)
+model = transformer.Transformer(
+    num_hid=200,
+    num_head=4,
+    num_feed_forward=400,
+    source_maxlen = dp.FRAME_LEN,
+    target_maxlen=64,
+    num_layers_enc=2,
+    num_layers_dec=1,
+    num_classes=62
+)
+loss_fn = tf.keras.losses.CategoricalCrossentropy(
+    from_logits=True, label_smoothing=0.1,
+)
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+optimizers = [ Lion(), SGD()] #Adadelta, Lion Adamax(
+optmizerNames = ["Lion", "SGD"]
+results = []
+
+for i in range(len(optimizers)):
+    optimizer = optimizers[i]
+    model.compile(optimizer=optimizer, loss=loss_fn)
+        
+    history = model.fit(dp.train_ds, validation_data=dp.valid_ds, callbacks=[display_cb], epochs=3, verbose=1)
+    results.append(history)
     
-    def animate_func(i):
-        im.set_array(images[i])
-        return [im]
+plt.figure(figsize=(20, 15))
+for j in range(len(optimizers)):
+    plt.subplot(1, 2, 1)
+    history = results[j]
+    plt.plot(history.history['loss'], label=optmizerNames[j])
+    leg = plt.legend(loc='upper right')
+    plt.title("loss")
+    plt.xlabel('Time')
+    plt.ylabel('Accuracy')
 
-    return animation.FuncAnimation(fig, animate_func, frames=len(images), interval=1000/10)
-
-
-
-# This function extracts the data for both hands.
-def get_hands(seq_df):
-    images = []
-    all_hand_landmarks = []
-    for seq_idx in range(len(seq_df)):
-        x_hand = seq_df.iloc[seq_idx].filter(regex="x_right_hand.*").values
-        y_hand = seq_df.iloc[seq_idx].filter(regex="y_right_hand.*").values
-        z_hand = seq_df.iloc[seq_idx].filter(regex="z_right_hand.*").values
-
-        right_hand_image = np.zeros((600, 600, 3))
-
-        right_hand_landmarks = landmark_pb2.NormalizedLandmarkList()
-        
-        for x, y, z in zip(x_hand, y_hand, z_hand):
-            right_hand_landmarks.landmark.add(x=x, y=y, z=z)
-
-        mp_drawing.draw_landmarks(
-                right_hand_image,
-                right_hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style())
-        
-        x_hand = seq_df.iloc[seq_idx].filter(regex="x_left_hand.*").values
-        y_hand = seq_df.iloc[seq_idx].filter(regex="y_left_hand.*").values
-        z_hand = seq_df.iloc[seq_idx].filter(regex="z_left_hand.*").values
-        
-        left_hand_image = np.zeros((600, 600, 3))
-        
-        left_hand_landmarks = landmark_pb2.NormalizedLandmarkList()
-        for x, y, z in zip(x_hand, y_hand, z_hand):
-            left_hand_landmarks.landmark.add(x=x, y=y, z=z)
-
-        mp_drawing.draw_landmarks(
-                left_hand_image,
-                left_hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style())
-        
-        images.append([right_hand_image.astype(np.uint8), left_hand_image.astype(np.uint8)])
-        all_hand_landmarks.append([right_hand_landmarks, left_hand_landmarks])
-    return images, all_hand_landmarks
-
-
-#images, labels, labels2=common.read_json('D:\asl-fingerspelling\character_to_prediction_index.json', 20)
-dataset_df = pd.read_csv("D:\\asl-fingerspelling\\train.csv")
-print("Full train dataset shape is {}".format(dataset_df.shape))
-
-sequence_id, file_id, phrase = dataset_df.iloc[0][['sequence_id', 'file_id', 'phrase']]
-print(f"sequence_id: {sequence_id}, file_id: {file_id}, phrase: {phrase}")
-
-# Fetch data from parquet file
-sample_sequence_df = pq.read_table(f"D:\\asl-fingerspelling/train_landmarks/{str(file_id)}.parquet",
-filters=[[('sequence_id', '=', sequence_id)],]).to_pandas()
-print("Full sequence dataset shape is {}".format(sample_sequence_df.shape))
-print(sample_sequence_df.head())
-
-# Extract the landmark data and convert it to an image using medipipe library.
-mp_pose = mediapipe.solutions.pose
-mp_hands = mediapipe.solutions.hands
-mp_drawing = mediapipe.solutions.drawing_utils 
-mp_drawing_styles = mediapipe.solutions.drawing_styles
-
-# Get the images created using mediapipe apis
-hand_images, hand_landmarks = get_hands(sample_sequence_df)
-# Fetch and show the data for right hand
-create_animation(np.array(hand_images)[:, 0])
-
-# Pose coordinates for hand movement.
-LPOSE = [13, 15, 17, 19, 21]
-RPOSE = [14, 16, 18, 20, 22]
-POSE = LPOSE + RPOSE
-
-X = [f'x_right_hand_{i}' for i in range(21)] + [f'x_left_hand_{i}' for i in range(21)] + [f'x_pose_{i}' for i in POSE]
-Y = [f'y_right_hand_{i}' for i in range(21)] + [f'y_left_hand_{i}' for i in range(21)] + [f'y_pose_{i}' for i in POSE]
-Z = [f'z_right_hand_{i}' for i in range(21)] + [f'z_left_hand_{i}' for i in range(21)] + [f'z_pose_{i}' for i in POSE]
-FEATURE_COLUMNS = X + Y + Z
-
-X_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "x_" in col]
-Y_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "y_" in col]
-Z_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "z_" in col]
-
-RHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if "right" in col]
-LHAND_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "left" in col]
-RPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in RPOSE]
-LPOSE_IDX = [i for i, col in enumerate(FEATURE_COLUMNS)  if  "pose" in col and int(col[-2:]) in LPOSE]
-
-
-#Treating data in the dataset
-# Set length of frames to 128
-FRAME_LEN = 128
-
-# Create directory to store the new data
-if not os.path.isdir("D:\\asl-fingerspelling\\preprocessed"):
-    os.mkdir("D:\\asl-fingerspelling\\preprocessed")
-else:
-    shutil.rmtree("D:\\asl-fingerspelling\\preprocessed")
-    os.mkdir("D:\\asl-fingerspelling\\preprocessed")
-
-# Loop through each file_id
-for file_id in tqdm(dataset_df.file_id.unique()):
-    # Parquet file name
-    pq_file = f"D:\\asl-fingerspelling\\train_landmarks\\{file_id}.parquet"
-    # Filter train.csv and fetch entries only for the relevant file_id
-    file_df = dataset_df.loc[dataset_df["file_id"] == file_id]
-    # Fetch the parquet file
-    parquet_df = pq.read_table(f"D:\\asl-fingerspelling\\train_landmarks\\{str(file_id)}.parquet",
-                              columns=['sequence_id'] + FEATURE_COLUMNS).to_pandas()
-    # File name for the updated data
-    tf_file = f"D:\\asl-fingerspelling\\preprocessed/{file_id}.tfrecord"
-    print(tf_file)
-    parquet_numpy = parquet_df.to_numpy()
-    # Initialize the pointer to write the output of 
-    # each `for loop` below as a sequence into the file.
-    with tf.io.TFRecordWriter(tf_file) as file_writer:
-        # Loop through each sequence in file.
-        for seq_id, phrase in zip(file_df.sequence_id, file_df.phrase):
-            # Fetch sequence data
-            frames = parquet_numpy[parquet_df.index == seq_id]
-            
-            # Calculate the number of NaN values in each hand landmark
-            r_nonan = np.sum(np.sum(np.isnan(frames[:, RHAND_IDX]), axis = 1) == 0)
-            l_nonan = np.sum(np.sum(np.isnan(frames[:, LHAND_IDX]), axis = 1) == 0)
-            no_nan = max(r_nonan, l_nonan)
-            
-            if 2*len(phrase)<no_nan:
-                features = {FEATURE_COLUMNS[i]: tf.train.Feature(
-                    float_list=tf.train.FloatList(value=frames[:, i])) for i in range(len(FEATURE_COLUMNS))}
-                features["phrase"] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[bytes(phrase, 'utf-8')]))
-                record_bytes = tf.train.Example(features=tf.train.Features(feature=features)).SerializeToString()
-                file_writer.write(record_bytes)
-                
-tf_records = dataset_df.file_id.map(lambda x: f'D:\\asl-fingerspelling\\preprocessed\\{x}.tfrecord').unique()
-print(f"List of {len(tf_records)} TFRecord files.")
+for k in range(len(optimizers)):
+    plt.subplot(1, 2, 2)
+    history = results[k]
+    plt.plot(history.history['val_loss'], label=optmizerNames[k])
+    leg = plt.legend(loc='upper right')
+    plt.title("Training loss")
+    plt.xlabel('Time')
+    plt.ylabel('Accuracy')
+   
+plt.show()
